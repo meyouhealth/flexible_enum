6ea3ec236693d2a9f118c4b0b55388dff5f54bac
Alex Robbin <alex.robbin@meyouhealth.com>
2013-10-21 09:54:22 -0400

    if corresponding timestamp attributes exist, update them

--- lib/flexible_enum/bang_method_configurator.rb
+++ lib/flexible_enum/bang_method_configurator.rb
@@ -5,7 +5,12 @@ module FlexibleEnum
         class_for_attribute.instance_eval do
           bang_method_name = element_config[:setter] || "#{element_name}!"
           define_method(bang_method_name) do
-            update_attribute(attribute_name, element_config[:value])
+            attributes = {attribute_name => element_config[:value]}
+            timestamp_attribute_name = element_config[:timestamp_attribute] || element_name
+            time = Time.now.utc
+            attributes["#{timestamp_attribute_name}_on".to_sym] = time.to_date if class_for_attribute.attribute_method?("#{timestamp_attribute_name}_on")
+            attributes["#{timestamp_attribute_name}_at".to_sym] = time if class_for_attribute.attribute_method?("#{timestamp_attribute_name}_at")
+            update_attributes(attributes)
           end
         end
       end
