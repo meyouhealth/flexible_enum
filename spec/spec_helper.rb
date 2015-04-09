6ece962ef8e42fa67f281cb909162009563ad310
Matt Daubert <matt.daubert@meyouhealth.com>
2015-04-09 10:02:49 -0400

    Do not stub ActiveRecord in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,15 @@ require 'active_record'
 
 ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
 
+RSpec.configure do |config|
+  config.around(:each) do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+      raise ActiveRecord::Rollback
+    end
+  end
+end
+
 ActiveRecord::Schema.define do
   create_table "cash_registers" do |t|
     t.integer  "status"
